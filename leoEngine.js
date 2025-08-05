edbe28a3089aa95546330542653bd09529905e2d
Leandro Gabriel - 2045189 <leandro.gabriel@ifro.edu.br>
2025-08-05 15:42:27 -0400

    Inclusão de novas funções de desenho, e métodos de renderização de primitivos

--- leoEngine.js
+++ leoEngine.js
@@ -3,25 +3,25 @@ var canvas = Element;
 var ctx = CanvasRenderingContext2D;
 console.log("START LeoWebEngine 0.0.0.1");
 const C = {
-	WHITE: "#FFFFFF",
-	RED: "#FF0000",
-	GREEN: "#00FF00",
-	BLUE: "#0000FF",
-	YELLOW: "#FFFF00",
-	BLACK: "#000000",
-	GRAY: "#808080",
-	GREY: "#808080",
-	SILVER: "#C0C0C0",
-	MAROON: "#800000",
-	OLIVE: "#808000",
-	LIME: "#00FF00",
-	AQUA: "#00FFFF",
-	TEAL: "#008080",
-	NAVY: "#000080",
-	FUCHSIA: "#FF00FF",
-	PURPLE: "#800080",
-	ORANGE: "#FFA500",
-	BROWN: "#A52A2A"
+	BLACK:		"#000000",
+	GRAY:		"#808080",
+	GREY:		"#808080",
+	SILVER:		"#C0C0C0",
+	WHITE:		"#FFFFFF",
+	MAROON:		"#800000",
+	RED:		"#FF0000",
+	BROWN:		"#A52A2A",
+	ORANGE:		"#FFA500",
+	OLIVE:		"#808000",
+	YELLOW:		"#FFFF00",
+	GREEN:		"#008000",
+	LIME:		"#00FF00",
+	TEAL:		"#008080",
+	AQUA:		"#00FFFF",
+	NAVY:		"#000080",
+	BLUE:		"#0000FF",
+	PURPLE:		"#800080",
+	FUCHSIA:	"#FF00FF",
 };
 const AL = {
 	LEFT: "left",
@@ -32,7 +32,15 @@ const AL = {
 	BOTTOM: "bottom",
 	NONE: "none"
 }
-
+const PR = {
+	LINELIST: "linelist",
+	LINESTRIP: "linestrip",
+	TRIANGLELIST: "trianglelist",
+	TRIANGLESTRIP: "trianglestrip",
+	TRIANGLEFAN: "trianglefan",
+	POINTLIST: "pointlist",
+	VERTEXMESH: "vertexmesh",
+}
 //Classes
 class Loader {
 	constructor() {
@@ -347,6 +355,19 @@ function drawSprite(argSprite,argX,argY) {
 		argX - argSprite.xOrigin, argY - argSprite.yOrigin, argSprite.w, argSprite.h
 	);
 };
+function drawSpriteTiled(argSprite,argX,argY) {
+	let x = argX - argSprite.xOrigin;
+	let y = argY - argSprite.yOrigin;
+	for (let i = 0; i < canvas.width; i += argSprite.w) {
+		for (let j = 0; j < canvas.height; j += argSprite.h) {
+			ctx.drawImage(
+				argSprite.img,
+				argSprite.x, argSprite.y, argSprite.w, argSprite.h,
+				i + x, j + y, argSprite.w, argSprite.h
+			);
+		}
+	}
+}
 /**
  * Desenha uma sprite com parâmetros estendidos (escala, rotação, blend, alpha).
  * @param {Sprite} argSprite - Sprite a ser desenhada.
@@ -399,11 +420,171 @@ function drawSpriteExt(argSprite, argX, argY, argScaleX = 1, argScaleY = 1, argR
 
     ctx.restore();
 }
+// Desenhar uma sprite com todos os parâmetros possíveis ajustáveis, desde o ponto XY até o ponto de origem e o ponto de destino, inclusive o ponto de offset para rotação e escala.
+function drawSpriteGeneral(argSprite, argPS1, argPS2, argPD1, argPD2, argScaleX = 1, argScaleY = 1, argRot = 0, argBlend = C.WHITE, argAlpha = 1) {
+	ctx.save();
+	//ctx.translate(argPD1.x, argPD1.y);
+	ctx.rotate(argRot);
+	//ctx.scale(argScaleX, argScaleY);
+	ctx.globalAlpha = argAlpha;
+
+	if (argBlend && argBlend !== C.WHITE) {
+
+		/* Blend ainda não funciona!
+		// Canvas auxiliar
+		let tempCanvas = document.createElement("canvas");
+		tempCanvas.width = argSprite.w;
+		tempCanvas.height = argSprite.h;
+		let tempCtx = tempCanvas.getContext("2d");
+
+		// Desenha a imagem
+		tempCtx.drawImage(
+			argSprite.img,
+			argSprite.x, argSprite.y, argSprite.w, argSprite.h,
+			0, 0, argSprite.w, argSprite.h
+		);
+
+		// Aplica o blend apenas nos pixels da imagem
+		tempCtx.globalCompositeOperation = "multiply";
+		tempCtx.fillStyle = argBlend;
+		tempCtx.fillRect(0, 0, argSprite.w, argSprite.h);
+
+		// Volta ao modo normal e desenha no contexto principal
+		ctx.drawImage(
+			tempCanvas,
+			-argP1.x + argP2.x, -argP1.y + argP2.y, argSprite.w, argSprite.h
+		);
+		*/
+	} else {
+		ctx.drawImage(
+			argSprite.img,
+			argPS1.x, argPS1.y, argPS2.x - argPS1.x, argPS2.y - argPS1.y,
+			argPD1.x, argPD1.y, (argPD2.x - argPD1.x) * argScaleX, (argPD2.y - argPD1.y) * argScaleY
+		);
+	}
+
+	ctx.restore();
+
+}
+
 function drawLine(argPos1,argPos2) {
 	ctx.beginPath();
 	ctx.moveTo(argPos1.x,argPos1.y);
 	ctx.lineTo(argPos2.x,argPos2.y);
 	ctx.stroke();
+}
+var _PRDraw = null;
+var _vertices = [];
+/**
+ * Inicia o desenho de um primitivo
+ * @param {PR} argPR Formato de desenho do primitivo
+ */
+function drawPrimitiveBegin(argPR) {
+	_PRDraw = argPR;
+	ctx.beginPath();
+}
+function drawVertex(argPos) {
+	argPos.x = Math.floor(argPos.x);
+	argPos.y = Math.floor(argPos.y);
+	argPos.x -= 0.5;
+	argPos.y -= 0.5;
+	_vertices.push(argPos);
+}
+function drawPrimitiveEnd() {
+	switch (_PRDraw) {
+		case PR.LINELIST:
+			// Desenha pares de vértices como linhas independentes
+			for (let i = 0; i < _vertices.length - 1; i += 2) {
+				ctx.moveTo(_vertices[i].x, _vertices[i].y);
+				ctx.lineTo(_vertices[i + 1].x, _vertices[i + 1].y);
+			}
+			ctx.stroke();
+			break;
+		case PR.LINESTRIP:
+			// Desenha uma linha conectando todos os vértices em sequência
+			if (_vertices.length > 1) {
+				ctx.moveTo(_vertices[0].x, _vertices[0].y);
+				for (let i = 1; i < _vertices.length; i++) {
+					ctx.lineTo(_vertices[i].x, _vertices[i].y);
+				}
+				ctx.stroke();
+			}
+			break;
+		case PR.TRIANGLELIST:
+			// Desenha triângulos independentes a cada grupo de 3 vértices
+			for (let i = 0; i < _vertices.length - 2; i += 3) {
+				ctx.beginPath();
+				ctx.moveTo(_vertices[i].x, _vertices[i].y);
+				ctx.lineTo(_vertices[i + 1].x, _vertices[i + 1].y);
+				ctx.lineTo(_vertices[i + 2].x, _vertices[i + 2].y);
+				ctx.closePath();
+				ctx.fill();
+			}
+			break;
+		case PR.TRIANGLESTRIP:
+			// Desenha triângulos conectados em strip
+			for (let i = 0; i < _vertices.length - 2; i++) {
+				ctx.beginPath();
+				ctx.moveTo(_vertices[i].x, _vertices[i].y);
+				ctx.lineTo(_vertices[i + 1].x, _vertices[i + 1].y);
+				ctx.lineTo(_vertices[i + 2].x, _vertices[i + 2].y);
+				ctx.closePath();
+				ctx.fill();
+			}
+			break;
+		case PR.TRIANGLEFAN:
+			// Desenha triângulos conectados em fan
+			if (_vertices.length > 2) {
+				for (let i = 1; i < _vertices.length - 1; i++) {
+					ctx.beginPath();
+					ctx.moveTo(_vertices[0].x, _vertices[0].y);
+					ctx.lineTo(_vertices[i].x, _vertices[i].y);
+					ctx.lineTo(_vertices[i + 1].x, _vertices[i + 1].y);
+					ctx.closePath();
+					ctx.fill();
+				}
+			}
+			break;
+		case PR.POINTLIST:
+			// Desenha pontos
+			for (let v of _vertices) {
+				ctx.beginPath();
+				ctx.arc(v.x, v.y, 2, 0, Math.PI * 2);
+				ctx.fill();
+			}
+			break;
+		case PR.VERTEXMESH:
+			ctx.beginPath();
+			ctx.moveTo(_vertices[0].x, _vertices[0].y);
+			for (let i = 1; i < _vertices.length; i++) {
+				ctx.lineTo(_vertices[i].x, _vertices[i].y);
+			}
+			ctx.closePath();
+			ctx.fill();
+			break;
+		default:
+			break;
+	}
+	_vertices = [];
+}
+var _color = "#000000";
+/**
+ * Define a cor para funções de desenho
+ * @param {string} argC A cor (em hex) para definir futuras funções de desenho
+ */
+function drawSetColor(argC) {
+	_color = argC;
+	ctx.fillStyle = _color;
+	ctx.strokeStyle = _color;
+}
+function makeColorRGB(argR, argG, argB) {
+	let r = Math.max(0, Math.min(255, argR));
+	let g = Math.max(0, Math.min(255, argG));
+	let b = Math.max(0, Math.min(255, argB));
+	return "#" + ((1 << 24) + (r << 16) + (g << 8) + b)
+		.toString(16)
+		.slice(1)
+		.toUpperCase();
 }
 function createWindow(argW,argH,argTitle="") {
 	let newWindow = new RenderWindow(argW,argH,argTitle);
